784db74f01ac3954f29c6694db9144596509899b
Michael Hotwagner <mhotwagner@gmail.com>
2018-03-04 20:54:34 -0500

    Add contiguous_sum and broken test

---
+++ contiguous_sum.py
@@ -0,0 +1,11 @@
+
+
+# max_contiguous_sum([-2, -3, 4, -1, -2, 1, 5, -3]) -> 7
+
+
+def max_contiguous_sum(numbers: list) -> int:
+    return 0
+
+
+def test_max_contiguous_sum() -> None:
+    assert max_contiguous_sum([-2, -3, 4, -1, -2, 1, 5, -3]) == 7
---
+++ tests/test_contiguous_sum.py
@@ -0,0 +1,5 @@
+from contiguous_sum import max_contiguous_sum
+
+
+def test_max_contiguous_sum() -> None:
+    assert max_contiguous_sum([-2, -3, 4, -1, -2, 1, 5, -3]) == 7
