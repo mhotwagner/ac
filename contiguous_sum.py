1be0a495f24553ac2720652e1d11fd596e7f1289
Michael Hotwagner <mhotwagner@gmail.com>
2018-03-04 21:25:50 -0500

    Make max_contiguous array work

--- contiguous_sum.py
+++ contiguous_sum.py
@@ -4,8 +4,32 @@
 
 
 def max_contiguous_sum(numbers: list) -> int:
-    return 0
+    highest = numbers[0]
+    local_highest = highest
+
+    for index, x in enumerate(numbers[1:]):
+        local_highest += x
+        if x > local_highest:
+            local_highest = x
+        if local_highest > highest:
+            highest = local_highest
+
+    return highest
 
 
-def test_max_contiguous_sum() -> None:
-    assert max_contiguous_sum([-2, -3, 4, -1, -2, 1, 5, -3]) == 7
+def max_contiguous_sub_array(numbers: list) -> list:
+    i, f = 0, 1
+    highest = numbers[0]
+    local_highest = highest
+
+    for index, x in enumerate(numbers[1:]):
+        index += 1
+        local_highest += x
+        if x > local_highest:
+            local_highest = x
+            i = index
+        if local_highest > highest:
+            highest = local_highest
+            f = index + 1
+
+    return numbers[i:f]
--- tests/test_contiguous_sum.py
+++ tests/test_contiguous_sum.py
@@ -1,5 +1,13 @@
-from contiguous_sum import max_contiguous_sum
+from contiguous_sum import max_contiguous_sum, max_contiguous_sub_array
 
 
 def test_max_contiguous_sum() -> None:
     assert max_contiguous_sum([-2, -3, 4, -1, -2, 1, 5, -3]) == 7
+
+
+def test_max_contiguous_sub_array() -> None:
+    assert max_contiguous_sub_array([-2, -3, 4, -1, -2, 1, 5, -3]) == [4, -1, -2, 1, 5]
+
+
+def test_weird_max_contiguous_sub_array() -> None:
+    assert max_contiguous_sub_array([10, -1, -1]) == [10]
